936e89b64cd8ace4e83392168d8ffc04565483ea
rickard.wredberg@gmail.com <rickard.wredberg@gmail.com>
2026-09-22 10:32:18 +0200

    feat: hotel booking

---
+++ TheOOPHotel/HotelBooking.cs
@@ -0,0 +1,22 @@
+﻿namespace TheOOPHotel;
+
+internal class HotelBooking
+{
+    internal string GuestName {  get; set; }
+    internal DateTime StartDate { get; set; }
+    internal DateTime EndDate { get; set; }
+
+    public HotelBooking(string guestName, DateTime startDate, int lengthOfStayInDays)
+    {
+        GuestName = guestName;
+        StartDate = startDate;
+        EndDate = startDate.AddDays(lengthOfStayInDays);
+    }
+
+    internal void DisplayBookingInfo()
+    {
+        Console.WriteLine($"Name: {GuestName}");
+        Console.WriteLine($"Start Date: {StartDate}");
+        Console.WriteLine($"End Date: {EndDate}");
+    }
+}
