caf8ab64ea48a8b560d5ab99b4b78627b697f084
rickard.wredberg@gmail.com <rickard.wredberg@gmail.com>
2026-09-22 09:59:51 +0200

    Add project files.

---
+++ TheOOPHotel/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TheOOPHotel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
