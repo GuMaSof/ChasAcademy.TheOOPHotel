2b7b02bce7dca02997588c485cd46a1b97c4b8b1
Sahlberg96 <daniel.sahlberg@hotmail.com>
2026-09-22 10:43:55 +0200

    update

--- TheOOPHotel/Program.cs
+++ TheOOPHotel/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var hotelBooking = new HotelBooking("s",DateTime.Now, 3);
+
+            hotelBooking.DisplayBookingInfo();
+
+            hotelBooking.GuestName = Console.ReadLine();
+        }
+
+        static string GetNameInput()
+        {
+
+            return "";
         }
     }
 }
